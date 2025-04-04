a153bfc8a29835b90afbf8ec5bfe57825cea11a2
Khawaja Abdul Hafeez <54060296+Khhafeez47@users.noreply.github.com>
2025-04-05 01:25:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'));
 
 // Route to handle the default page
 app.get('/', function(request, response) {
-  response.send('Hello World this app is deployed by Kahafeez!');
+  response.send('Hello World this app is deployed by Khawaja Abdul Hafeez!');
 });
 
 // Listen on all interfaces (0.0.0.0), not just localhost
